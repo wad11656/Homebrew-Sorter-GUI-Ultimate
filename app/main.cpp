bca1482d1096e0f0c1f4984735f71ef5e83be79c
Wade Murdock <wad11656@gmail.com>
2026-04-30 21:19:38 -0600

    Increase heap size to 16 MB

--- app/main.cpp
+++ app/main.cpp
@@ -4,7 +4,7 @@
 
 PSP_MODULE_INFO("Homebrew Sorter Ultimate", 0x800, 1, 0);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
-PSP_HEAP_SIZE_KB(4096);
+PSP_HEAP_SIZE_KB(16384);
 
 int main(int argc, char* argv[]) {
     const char* execPath = (argc > 0) ? argv[0] : nullptr;
